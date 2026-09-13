6cb95a61d45d1b1d78094293aca77710bb798a8a
angirakushwaha513 <angirakushwaha513@gmail.com>
2026-09-13 08:32:04 +0530

    Update booking-status.js

--- booking-status.js
+++ booking-status.js
@@ -8,8 +8,8 @@
    API CONFIGURATION
 ========================================================= */
 
-const API_BASE_URL = "http://localhost:3000";
-
+const API_BASE_URL =
+    "https://neelmani-dj-booking.onrender.com";
 
 /* =========================================================
    DOM ELEMENTS
